9f230cc1f32981e098ce7e3f6fcd20a86425a0df
yurshtinkai <lourdangeloubufete17@gmail.com>
2025-08-29 12:55:35 +0800

    commit

--- src/app.ts
+++ src/app.ts
@@ -33,7 +33,7 @@ const PORT = process.env.PORT || 5000;
 const isProduction = process.env.NODE_ENV === 'production';
 
 app.set('json spaces', 2);
-
+  
 // --- Global Middleware ---
 // CORS configuration for production
 const corsOptions = {
